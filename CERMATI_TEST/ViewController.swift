c4207dc1fe68bdcb35dee319ae74c5882eb70c11
Ernando Kasaluhe <ernandokasaluhe@Ernandos-MacBook-Pro.local>
2019-06-02 20:06:48 +0700

    cermati test first commit

---
+++ CERMATI_TEST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CERMATI_TEST
+//
+//  Created by Ernando Kasaluhe on 02/06/19.
+//  Copyright © 2019 Ernando Kasaluhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
